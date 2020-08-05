a89f2a32d2d80712a763018e571d18a02e1d9896
Ryan Lee <ralee478.school@gmail.com>
2020-08-05 15:20:43 -0700

    Added skeleton code for main Waitlist Data Struct

---
+++ Code/src/main/java/WaitlistQueue.java
@@ -0,0 +1,18 @@
+public class WaitlistQueue<T> {
+
+    public WaitlistQueue() {
+
+    }
+
+    public void addStudent() {
+
+    }
+
+    public T removeStudent() {
+
+    }
+
+    public String takeOffWaitlist() {
+
+    }
+}
